78d0c0c5f09a8b467ea9653f401cd4e7a0d097fe
jiongrong fan <1446418561@qq.com>
2018-07-08 10:50:23 +0800

    dev 初次提交，附个人中心文件

---
+++ src/pages/personal/personalHome/PersonalHome.vue
@@ -0,0 +1,72 @@
+<!--
+Description
+@authors Your Name (you@example.org)
+@date    2018-07-06 20:13:34
+@version 1.0.0
+-->
+<template>
+    <div id="personal">
+        <view-box style="height:100%;" ref="viewBox">
+            <x-header slot="header" :right-options="{showMore: true}" @on-click-more="showMenus = true">
+                我的
+            </x-header>
+            <router-view></router-view>
+            <group class="clearfix">
+                <div class="avator ml20">
+                    <img src="../../../images/home/Home.png">
+                </div>
+                <div class="text-info ml20">
+                    <p>范炯荣</p>
+                    <p>155****2614</p>
+                </div>
+            </group>
+            <divider></divider>
+            <cellBox is-link link="/personal/order">我的订单</cellBox>
+            <tabbar slot="bottom">
+                <tabbar>
+                    <tabbar-item>
+                        <img class="tab-icon" slot="icon" src="../../../images/home/Home.png">
+                        <span slot="label">首页</span>
+                    </tabbar-item>
+                    <tabbar-item>
+                        <!-- <router-link :to="{path:'/personal/order'}"> -->
+                        <img class="tab-icon" slot="icon" src="../../../images/personal/person.png">
+                        <span slot="label">个人中心</span>
+                        <!-- </router-link> -->
+                    </tabbar-item>
+
+                </tabbar>
+            </tabbar>
+        </view-box>
+    </div>
+</template>
+
+
+<script>
+import {
+  ViewBox,
+  XHeader,
+  Group,
+  Cell,
+  CellBox,
+  Tabbar,
+  TabbarItem,
+  Divider
+} from "vux";
+export default {
+  components: {
+    ViewBox,
+    XHeader,
+    Group,
+    Cell,
+    CellBox,
+    Tabbar,
+    TabbarItem,
+    Divider
+  }
+};
+</script>
+
+<style lang="less" scoped>
+@import "./PersonalHome.less";
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,32 +1,29 @@
 import Vue from 'vue';
 import Router from 'vue-router';
 
+import PersonalHome from '../pages/personal/personalHome/PersonalHome';
+import OrderList from '../pages/personal/orderList/OrderList';
+
 const Home = resolve => require(['@/pages/home/Home.vue'], resolve);
+
+
 Vue.use(Router);
 
 const router = new Router({
     routes: [{
         path: '/',
         name: 'home',
-        // redirect: '/homePage',
         alias: ['/index'],
-        component: Home
-    }, {
+        component: PersonalHome
+    },{
+        path: '/personal/order',
+        name: 'order',
+        component: OrderList
+    },{
         path: '*',
         redirect: '/'
     }]
 });
-/*router.beforeEach((to,from,next)=>{
-    if(to.meta.requireAuth){
-        if(getStore('token')){
-            next();
-        }else{
-            next();
-            // location.href="http://172.16.16.122:8080/security";
-        }
-    }else{
-        next();
-    }
-});*/
 
 export default router;
+                                        
