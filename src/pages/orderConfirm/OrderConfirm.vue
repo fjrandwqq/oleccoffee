464766d877b58bf51215148d2c71bd02be95c6ef
jiongrong fan <1446418561@qq.com>
2018-07-10 22:45:34 +0800

    确认订单一小部分代码

--- src/pages/orderConfirm/OrderConfirm.vue
+++ src/pages/orderConfirm/OrderConfirm.vue
@@ -1,16 +1,40 @@
 <style lang="less" scoped src="./OrderConfirm.less"></style>
 <template>
     <div id="order-confirm">
-        订单确认
+        <div class="taking-way"></div>
+        <div v-if="takingWay==0" class="buyer-info">
+            <div>
+            </div>
+            
+        </div>
+        <div v-if="takingWay==1" class="seller-info">
+
+        </div>
+        <div class="goods-info">
+
+        </div>
+        <div class="payForm">
+        </div>
+        <div class="footer-bar">
+            <div class="total-price">
+                <span>合计￥</span>
+                <span class="total">{{total}}</span>
+            </div>
+            <div class="pay-btn" @click="goPay">去支付</div>
+        </div>
     </div>
 </template>
 <script>
 export default {
-    data(){
-        return{
-
-        }
-    }
-}
+	data() {
+		return {
+			takingWay: '',
+			total: 10,
+		};
+	},
+};
 </script>
+<style lang="less" scoped src="./OrderConfirm.less"></style>
+
+
 
