85c5a5911112ef4487b937742927cbf0d98cab30
5201314999 <1446418561@qq.com>
2018-07-12 19:40:55 +0800

    确认订单

--- src/pages/orderConfirm/OrderConfirm.vue
+++ src/pages/orderConfirm/OrderConfirm.vue
@@ -5,13 +5,13 @@
 
         </div>
         <group v-if="order.takingWay==0" class="buyer-info">
-            <cell-box class="address-wrapper" is-link @click.native="showAddressModal">
+            <cell-box class="address-wrapper" is-link @click.native="showAddressPopup=true">
                 <div>
                     <p>白马岗小区11栋1梯7021111111111111111111111111</p>
                     <p>菊花战斗士 15218046490</p>
                 </div>
             </cell-box>
-            <cell-box class="time-wrapper" is-link @click.native="showTimeModal">
+            <cell-box class="time-wrapper" is-link @click.native="showTimePopup = true">
                 <p>送达时间</p>
                 <p>大约 {{order.servedTime}} 送达</p>
             </cell-box>
@@ -22,16 +22,11 @@
                     <p>广州市天河区景明街1号前座07店铺</p>
                 </div>
             </cell-box>
-            <cell-box class="time-wrapper" is-link @click.native="showTimeModal">
+            <cell-box class="time-wrapper" is-link @click.native="showTimePopup = true">
                 <p>自取时间</p>
                 <p>{{order.servedTime}}</p>
             </cell-box>
         </group>
-        <div v-transfer-dom>
-            <popup v-model="showTimePopup">
-                <datetime-view v-model="order.servedTime" format="HH:mm"></datetime-view>
-            </popup>
-        </div>
         <group class="goods-info">
             <cell-box>
                 <div class="flex-item left-info">
@@ -71,6 +66,32 @@
             </div>
             <div class="pay-btn" @click="goPay">去支付</div>
         </div>
+        <div v-transfer-dom>
+            <popup class="address-popup" v-model="showAddressPopup" height="270px" is-transparent>
+                <div style="background-color:#fff;height:250px;margin:0 auto;border-radius:5px;">
+                    <div class="header">
+                        <div class="header-left">取消</div>
+                        <div class="header-title">选择收货地址</div>
+                    </div>
+                    <group class="body" v-for="(address,index) in addressList">
+                        <div class="radio">
+                            
+                        </div>
+                        <div class="content">
+                            
+                        </div>
+                        <div class="btn">
+
+                        </div>
+                    </group>
+                </div>
+            </popup>
+        </div>
+        <div v-transfer-dom>
+            <popup v-model="showTimePopup">
+                <datetime-view v-model="order.servedTime" format="HH:mm"></datetime-view>
+            </popup>
+        </div>
     </div>
 </template>
 <script>
@@ -88,23 +109,25 @@
         data() {
             return {
                 showTimePopup: false,
+                showAddressPopup: false,
                 order: {
-                    takingWay: 1,
+                    takingWay: 0,
                     total: 10,
                     servedTime: '14:37',
                     discount: 0.8,
                     imgSrc: 'https://ww1.sinaimg.cn/large/663d3650gy1fq66vvsr72j20p00gogo2.jpg',
-                    realTotal: 20
+                    realTotal: 20,
                 },
+                addressList: [
+                    {
+                        address: '广东深圳龙岗区扬马小区振兴楼705',
+                        nickName: '骑单车的小员工',
+                        phone: '15647891122',
+                    }
+                ]
             };
         },
         methods: {
-            showAddressModal() {
-                alert('11');
-            },
-            showTimeModal() {
-                this.showTimePopup = true;
-            },
             goPay() {
 
             }
