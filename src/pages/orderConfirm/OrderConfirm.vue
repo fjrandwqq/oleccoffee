6db821b6ccdedb7be06d3245460be1dce5839677
jiongrong fan <1446418561@qq.com>
2018-07-14 19:28:11 +0800

    确认订单

--- src/pages/orderConfirm/OrderConfirm.vue
+++ src/pages/orderConfirm/OrderConfirm.vue
@@ -2,18 +2,21 @@
 <template>
     <div id="order-confirm">
         <div class="taking-way">
-
+            <template>
+            <div>外卖配送</div>
+            <div>到店自提</div>
+            </template>
         </div>
         <group v-if="order.takingWay==0" class="buyer-info">
-            <cell-box class="address-wrapper" is-link @click.native="showAddressPopup=true">
-                <div>
+            <cell-box class="address-wrapper" is-link @click.native="showAddressPopup">
+                <div class="pr15">
                     <p>白马岗小区11栋1梯7021111111111111111111111111</p>
                     <p>菊花战斗士 15218046490</p>
                 </div>
             </cell-box>
             <cell-box class="time-wrapper" is-link @click.native="showTimePopup = true">
                 <p>送达时间</p>
-                <p>大约 {{order.servedTime}} 送达</p>
+                <p class="pr15">大约 {{order.servedTime}} 送达</p>
             </cell-box>
         </group>
         <group v-else class="seller-info">
@@ -22,7 +25,7 @@
                     <p>广州市天河区景明街1号前座07店铺</p>
                 </div>
             </cell-box>
-            <cell-box class="time-wrapper" is-link @click.native="showTimePopup = true">
+            <cell-box class="time-wrapper" is-link @click.native="showAddressPopup">
                 <p>自取时间</p>
                 <p>{{order.servedTime}}</p>
             </cell-box>
@@ -38,19 +41,28 @@
                     </div>
                 </div>
                 <div class="price">
-                    ￥29
+                    <span class="symbol">￥</span>29
                 </div>
             </cell-box>
-            <cell-box v-if="order.discount">
-                <div class="flex-item">活动打{{order.discount*10}}折</div>
-                <div>￥5.8</div>
+            <cell-box class="discount" v-if="order.discount">
+                <div class="flex-item">
+                    <img src="../../images/discount.png">
+                    <span> 活动打{{order.discount*10}}折</span>
+                </div>
+                <div class="red">
+                    <span class="symbol">-￥</span>5.8</div>
             </cell-box>
-            <cell-box>
-                <div class="flex-item">配送费</div>
-                <div>￥5</div>
+            <cell-box class="service-pay">
+                <div class="flex-item ">配送费</div>
+                <div>
+                    <span class="symbol">￥</span>5</div>
             </cell-box>
             <cell-box class="total">
-                ￥{{order.realTotal}}
+                <div class="total-price">
+                    <span>小计</span>
+                    <span class="symbol">￥</span>
+                    <span class="total">{{order.total}}</span>
+                </div>
             </cell-box>
         </group>
         <group class="pay-form">
@@ -61,16 +73,17 @@
         </group>
         <div class="footer-bar">
             <div class="total-price">
-                <span>合计￥</span>
+                <span>合计</span>
+                <span class="symbol">￥</span>
                 <span class="total">{{order.total}}</span>
             </div>
             <div class="pay-btn" @click="goPay">去支付</div>
         </div>
         <div v-transfer-dom>
-            <popup class="address-popup" v-model="showAddressPopup" height="270px" is-transparent>
+            <popup class="address-popup" :hide-on-blur="false" height="18.75rem" v-model="addressPopup" is-transparent>
                 <div style="background-color:#fff;margin:0 auto;border-radius:5px;">
-                    <div class="header">
-                        <div class="header-left">取消</div>
+                    <div class="address-header">
+                        <div class="header-left" @click="closeAddressPopup">取消</div>
                         <div class="header-title">选择收货地址</div>
                     </div>
                     <div class="body" v-for="(item,index) in addressList" :key="index">
@@ -93,10 +106,13 @@
             </popup>
         </div>
         <div v-transfer-dom>
-            <popup class="edit-address-popup" v-model="showEditAddressPopup" height="270px" is-transparent>
+            <popup class="edit-address-popup" :hide-on-blur="false" v-model="editAddressPopup" is-transparent>
                 <div style="background-color:#fff;margin:0 auto;border-radius:5px;">
                     <group label-width="4.5rem" label-margin-right="2em" label-align="left">
-                        <x-header class="header" :left-options="{showBack: false}">选择收获地址</x-header> 
+                        <div class="address-header">
+                            <div class="header-left" @click="editAddressPopup=false">取消</div>
+                            <div class="header-title">选择收货地址</div>
+                        </div>
                         <x-input title="联系人" placeholder="必填" v-model="editAddress.contacts"></x-input>
                         <x-input title="手机号" placeholder="请填写收货人的手机号码" v-model="editAddress.phone"></x-input>
                         <x-input title="收获地址" placeholder="例：TIT创意园" v-model="editAddress.address"></x-input>
@@ -117,7 +133,7 @@
     </div>
 </template>
 <script>
-import { Group, CellBox, Popup, DatetimeView, XInput, XButton,XHeader, TransferDom } from 'vux';
+import { Group, CellBox, Popup, DatetimeView, XInput, XButton, XHeader, TransferDom } from 'vux';
 export default {
 	components: {
 		Group,
@@ -126,7 +142,7 @@ export default {
 		DatetimeView,
 		XInput,
 		XButton,
-        XHeader
+		XHeader,
 	},
 	directives: {
 		TransferDom,
@@ -134,8 +150,8 @@ export default {
 	data() {
 		return {
 			showTimePopup: false,
-			showAddressPopup: false,
-			showEditAddressPopup: false,
+			addressPopup: false,
+			editAddressPopup: false,
 			editAddress: {},
 			order: {
 				takingWay: 0,
@@ -145,26 +161,7 @@ export default {
 				imgSrc: 'https://ww1.sinaimg.cn/large/663d3650gy1fq66vvsr72j20p00gogo2.jpg',
 				realTotal: 20,
 			},
-			addressList: [
-				{
-					address: '广东深圳龙岗区扬马小区振兴楼',
-					contacts: '骑单车的小员工',
-					phone: '15647891122',
-					doorNumber: '12313123',
-				},
-				{
-					address: '广东深圳龙岗区扬马小区振兴楼',
-					contacts: '骑单车的小员工',
-					phone: '15647891122',
-					doorNumber: '12313123',
-				},
-				{
-					address: '广东深圳龙岗区扬马小区振兴楼',
-					contacts: '骑单车的小员工',
-					phone: '15647891122',
-					doorNumber: '12313123',
-				},
-			],
+			addressList: [],
 		};
 	},
 	methods: {
@@ -174,9 +171,77 @@ export default {
 				this.$router.push('addAddress');
 			});
 		},
+		showAddressPopup() {
+			this.addressList = [
+				{
+					address: '广东深圳龙岗区扬马小区振兴楼',
+					contacts: '骑单车的小员工',
+					phone: '15647891122',
+					doorNumber: '12313123',
+				},
+				{
+					address: '广东深圳龙岗区扬马小区振兴楼',
+					contacts: '骑单车的小员工',
+					phone: '15647891122',
+					doorNumber: '12313123',
+				},
+				{
+					address: '广东深圳龙岗区扬马小区振兴楼',
+					contacts: '骑单车的小员工',
+					phone: '15647891122',
+					doorNumber: '12313123',
+				},
+				{
+					address: '广东深圳龙岗区扬马小区振兴楼',
+					contacts: '骑单车的小员工',
+					phone: '15647891122',
+					doorNumber: '12313123',
+				},
+				{
+					address: '广东深圳龙岗区扬马小区振兴楼',
+					contacts: '骑单车的小员工',
+					phone: '15647891122',
+					doorNumber: '12313123',
+				},
+				{
+					address: '广东深圳龙岗区扬马小区振兴楼',
+					contacts: '骑单车的小员工',
+					phone: '15647891122',
+					doorNumber: '12313123',
+				},
+				{
+					address: '广东深圳龙岗区扬马小区振兴楼',
+					contacts: '骑单车的小员工',
+					phone: '15647891122',
+					doorNumber: '12313123',
+				},
+				{
+					address: '广东深圳龙岗区扬马小区振兴楼',
+					contacts: '骑单车的小员工',
+					phone: '15647891122',
+					doorNumber: '12313123',
+				},
+				{
+					address: '广东深圳龙岗区扬马小区振兴楼',
+					contacts: '骑单车的小员工',
+					phone: '15647891122',
+					doorNumber: '12313123',
+				},
+				{
+					address: '广东深圳龙岗区扬马小区振兴楼',
+					contacts: '骑单车的小员工',
+					phone: '15647891122',
+					doorNumber: '12313123',
+				},
+			];
+			this.addressPopup = true;
+		},
+		closeAddressPopup() {
+			this.addressPopup = false;
+		},
 		showEditPopup(index) {
-			this.showAddressPopup = false;
-			this.showEditAddressPopup = true;
+			this.addressPopup = false;
+			this.editAddressPopup = true;
 			this.editAddress = this.addressList[index];
 		},
 	},
