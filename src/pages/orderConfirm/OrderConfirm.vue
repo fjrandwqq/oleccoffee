6574608af7d51ae3e229919bf3d83e45386126c4
5201314999 <1446418561@qq.com>
2018-08-07 14:42:41 +0800

    加上打印出支付的回调

--- src/pages/orderConfirm/OrderConfirm.vue
+++ src/pages/orderConfirm/OrderConfirm.vue
@@ -304,14 +304,6 @@ export default {
 		 */
 		onBridgeReady(data) {
 			var vm = this;
-			const dd = {
-				appId: data.appId, //公众号名称，由商户传入
-				timeStamp: data.timeStamp, //时间戳，自1970年以来的秒数
-				nonceStr: data.nonceStr, //随机串
-				package: data.packageValue, // 统一支付接口返回的prepay_id参数值，提交格式如：prepay_id=\*\*\*）
-				signType: data.signType, //微信签名方式：
-				paySign: data.paySign, //微信签名
-			};
 			WeixinJSBridge.invoke(
 				'getBrandWCPayRequest',
 				{
@@ -323,6 +315,7 @@ export default {
 					paySign: data.paySign, //微信签名
 				},
 				function(res) {
+						vm.$vux.toast.show({ type: 'warn', text:res.err_msg  });
 					// 使用以上方式判断前端返回,微信团队郑重提示：res.err_msg将在用户支付成功后返回ok，但并不保证它绝对可靠。
 					if (res.err_msg == 'get_brand_wcpay_request：ok') {
 						vm.$vux.toast.show({ text: '成功' });
