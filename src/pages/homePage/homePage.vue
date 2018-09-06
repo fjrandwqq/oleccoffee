2ae980105e56e96deb149590cc45b750323623b3
jiongrong fan <1446418561@qq.com>
2018-09-06 23:49:01 +0800

    自动定位到最近店铺功能

--- src/pages/homePage/homePage.vue
+++ src/pages/homePage/homePage.vue
@@ -101,367 +101,391 @@
 	</div>
 </template>
 <script>
-	import BScroll from 'better-scroll';
-	import { Picker, Popup, TransferDom } from 'vux';
-	import { Swiper, Slide } from 'vue-swiper-component';
-	import SpecList from '@/components/SpecList/SpecList';
-	import ShopCart from '@/components/ShopCart/ShopCart';
-	import { fixPrice } from '@/services/utils';
-	import gpsCovert from '@/services/gpsConvert';
-	import {
-		getShopList,
-		getCategoryByShop,
-		getProductsByCategory,
-		getBanners,
-		getProductDetail,
-		getAllGoods,
-	} from '@/services/getData';
-	import { IMG_PATH, errorImgFunc, errorImg } from '@/config';
-	const scrollOption = {
-		click: true,
-		tap: true,
-	};
-	export default {
-		components: {
-			Picker,
-			Popup,
-			SpecList,
-			Swiper,
-			Slide,
-			ShopCart
+import BScroll from 'better-scroll';
+import { Picker, Popup, TransferDom } from 'vux';
+import { Swiper, Slide } from 'vue-swiper-component';
+import SpecList from '@/components/SpecList/SpecList';
+import ShopCart from '@/components/ShopCart/ShopCart';
+import { fixPrice } from '@/services/utils';
+import gpsCovert from '@/services/gpsConvert';
+import {
+	getShopList,
+	getCategoryByShop,
+	getProductsByCategory,
+	getBanners,
+	getProductDetail,
+	getAllGoods,
+} from '@/services/getData';
+import { IMG_PATH, errorImgFunc, errorImg } from '@/config';
+import { mapState } from 'vuex';
+const scrollOption = {
+	click: true,
+	tap: true,
+};
+export default {
+	components: {
+		Picker,
+		Popup,
+		SpecList,
+		Swiper,
+		Slide,
+		ShopCart,
+	},
+	directives: {
+		TransferDom,
+	},
+	computed: {
+		...mapState(['shopInfo']),
+	},
+	data() {
+		return {
+			errorImgFunc: errorImgFunc,
+			loading: false,
+			detailScroll: null,
+			productScroll: null,
+			products: [],
+			firstShowDetail: true,
+			categoryIndex: 0,
+			categories: [],
+			bannerList: [],
+			productModalShow: false,
+			count: 0,
+			selectProduct: {},
+			shopListPopup: false,
+			shopList: [['']],
+			selectShop: [],
+			address: '',
+			gpsPoint: null,
+			specListData: [],
+			cartModalShow: false,
+			goods: [],
+			listHeight: [],
+			categoryScroll: null,
+			scrollY: 0,
+			currentIndex: 0,
+			cartShow: false,
+			selectProducts: [
+				{
+					id: 0,
+					specialId: '0_大杯',
+					count: 3,
+					name: '测试',
+					specText: '大杯',
+					payPrice: 23,
+				},
+				{
+					count: 3,
+					name: '测试',
+					specText: '大杯',
+					payPrice: 23,
+				},
+				{
+					count: 3,
+					name: '测试',
+					specText: '大杯',
+					payPrice: 23,
+				},
+				{
+					count: 3,
+					name: '测试',
+					specText: '大杯',
+					payPrice: 23,
+				},
+				{
+					count: 3,
+					name: '测试',
+					specText: '大杯',
+					payPrice: 23,
+				},
+			],
+		};
+	},
+	computed: {
+		specListText() {
+			return this.specListData.map(i => i.selectSpec.name).join(' ');
 		},
-		directives: {
-			TransferDom,
+		extraPrice() {
+			return this.specListData.reduce((total, i) => fixPrice(i.selectSpec.moreMoney + total), 0);
 		},
-		data() {
-			return {
-				errorImgFunc: errorImgFunc,
-				loading: false,
-				detailScroll: null,
-				productScroll: null,
-				products: [],
-				firstShowDetail: true,
-				categoryIndex: 0,
-				categories: [],
-				bannerList: [],
-				productModalShow: false,
-				count: 0,
-				selectProduct: {},
-				shopListPopup: false,
-				shopList: [['']],
-				selectShop: [],
-				address: '',
-				gpsPoint: null,
-				specListData: [],
-				cartModalShow: false,
-				goods: [],
-				listHeight: [],
-				categoryScroll: null,
-				scrollY: 0,
-				currentIndex: 0,
-				cartShow: false,
-				selectProducts: [
-					{
-						id: 0,
-						specialId: '0_大杯',
-						count: 3,
-						name: '测试',
-						specText: '大杯',
-						payPrice: 23
-					}, {
-						count: 3,
-						name: '测试',
-						specText: '大杯',
-						payPrice: 23
-					}, {
-						count: 3,
-						name: '测试',
-						specText: '大杯',
-						payPrice: 23
-					}, {
-						count: 3,
-						name: '测试',
-						specText: '大杯',
-						payPrice: 23
-					}, {
-						count: 3,
-						name: '测试',
-						specText: '大杯',
-						payPrice: 23
-					}
-				]
-			};
+		totalPrice() {
+			const { realPrice = 0, price = 0 } = this.selectProduct;
+			return fixPrice((+realPrice + this.extraPrice) * this.count);
 		},
-		computed: {
-			specListText() {
-				return this.specListData.map(i => i.selectSpec.name).join(' ');
-			},
-			extraPrice() {
-				return this.specListData.reduce((total, i) => fixPrice(i.selectSpec.moreMoney + total), 0);
-			},
-			totalPrice() {
-				const { realPrice = 0, price = 0 } = this.selectProduct;
-				return fixPrice((+realPrice + this.extraPrice) * this.count);
-			},
-			originTotalPrice() {
-				const { realPrice = 0, price = 0 } = this.selectProduct;
-				return fixPrice((+price + this.extraPrice) * this.count);
+		originTotalPrice() {
+			const { realPrice = 0, price = 0 } = this.selectProduct;
+			return fixPrice((+price + this.extraPrice) * this.count);
+		},
+	},
+	watch: {
+		scrollY(val) {
+			for (let i = 0; i < this.listHeight.length; i++) {
+				let height1 = this.listHeight[i];
+				let height2 = this.listHeight[i + 1];
+				if (!height2 || (val >= height1 && val < height2)) {
+					this.followScroll(i);
+					break;
+				}
 			}
 		},
-		watch: {
-			scrollY(val) {
-				for (let i = 0; i < this.listHeight.length; i++) {
-					let height1 = this.listHeight[i];
-					let height2 = this.listHeight[i + 1];
-					if (!height2 || (val >= height1 && val < height2)) {
-						this.followScroll(i);
-						break;
-					}
-				}
-			},
-		},
-		methods: {
-			selectCategory(index, event) {
-				// this.categoryIndex = index;
-				// this.productScroll && this.productScroll.scrollTo(0, 0, 500);
-				// categoryId &&
-				// 	getProductsByCategory(this.selectShop[0], categoryId).then(res => {
-				// 		res = res || [];
-				// 		this.products = res.map(i => {
-				// 			i.showImg = IMG_PATH + i.imgs.split(',')[0];
-				// 			return i;
-				// 		});
-				// 		this.$nextTick(() => {
-				// 			this.productScroll && this.productScroll.refresh();
-				// 		});
-				// 	});
+	},
+	methods: {
+		selectCategory(index, event) {
+			// this.categoryIndex = index;
+			// this.productScroll && this.productScroll.scrollTo(0, 0, 500);
+			// categoryId &&
+			// 	getProductsByCategory(this.selectShop[0], categoryId).then(res => {
+			// 		res = res || [];
+			// 		this.products = res.map(i => {
+			// 			i.showImg = IMG_PATH + i.imgs.split(',')[0];
+			// 			return i;
+			// 		});
+			// 		this.$nextTick(() => {
+			// 			this.productScroll && this.productScroll.refresh();
+			// 		});
+			// 	});
 
-				if (!event._constructed) {
-					return;
+			if (!event._constructed) {
+				return;
+			}
+			let productList = this.$refs.productList;
+			let el = productList[index];
+			this.productScroll.scrollToElement(el, 300);
+		},
+		minus() {
+			if (this.count > 1) --this.count;
+		},
+		add() {
+			if (this.count < 999) ++this.count;
+		},
+		goPay() {
+			this.productModalShow = false;
+			const orderGoods = [
+				{
+					goodsId: this.selectProduct.id,
+					goodsName: this.selectProduct.name,
+					goodsNum: this.count,
+					extraPrice: this.extraPrice,
+					price: this.selectProduct.price,
+					realPrice: this.selectProduct.realPrice,
+					discount: this.selectProduct.discount,
+					imgs: this.selectProduct.imgs,
+					totalPrice: this.totalPrice,
+					specListText: this.specListText,
+					specList: this.specListData.map(i => ({
+						name: i.selectSpec.name,
+						type: i.type,
+					})),
+				},
+			];
+			this.$nextTick(() => {
+				this.$router.push({ name: 'orderConfirm', params: { orderGoods: orderGoods } });
+			});
+		},
+		showShopPopup() {
+			this.shopListPopup = true;
+		},
+		showProductModal(product) {
+			if (product.statusName === '售罄') {
+				return;
+			}
+			const productId = product.id;
+			this.loading = this.productModalShow = true;
+			this.detailScroll && this.detailScroll.scrollTo(0, 0, 500);
+			getProductDetail(productId).then(res => {
+				this.selectProduct = res || { spec: [], imgs: '' };
+				this.selectProduct.img =
+					this.selectProduct.imgs && this.selectProduct.imgs.length > 0
+						? IMG_PATH + this.selectProduct.imgs.split(',')[0]
+						: errorImg;
+				let obj = {};
+				this.specListData = [];
+				res.spec.forEach(e => {
+					const { type } = e;
+					if (!obj[type]) obj[type] = { type, list: [] };
+					obj[type].list.push(e);
+				});
+
+				for (let i in obj) {
+					let data = obj[i];
+					let selectSpec = data.list.find(e => {
+						return e.moreMoney === 0;
+					});
+					// data.selectSpec = data.list[0];
+					data.selectSpec = selectSpec;
+					this.specListData.push(data);
 				}
-				let productList = this.$refs.productList;
-				let el = productList[index];
-				this.productScroll.scrollToElement(el, 300);
-			},
-			minus() {
-				if (this.count > 1)--this.count;
-			},
-			add() {
-				if (this.count < 999)++this.count;
-			},
-			goPay() {
-				this.productModalShow = false;
-				const orderGoods = [
-					{
-						goodsId: this.selectProduct.id,
-						goodsName: this.selectProduct.name,
-						goodsNum: this.count,
-						extraPrice: this.extraPrice,
-						price: this.selectProduct.price,
-						realPrice: this.selectProduct.realPrice,
-						discount: this.selectProduct.discount,
-						imgs: this.selectProduct.imgs,
-						totalPrice: this.totalPrice,
-						specListText: this.specListText,
-						specList: this.specListData.map(i => ({
-							name: i.selectSpec.name,
-							type: i.type,
-						})),
-					},
-				];
+				this.count = 1;
+				this.loading = false;
 				this.$nextTick(() => {
-					this.$router.push({ name: 'orderConfirm', params: { orderGoods: orderGoods } });
+					this.detailScroll && this.detailScroll.refresh();
 				});
-			},
-			showShopPopup() {
-				this.shopListPopup = true;
-			},
-			showProductModal(product) {
-				if(product.statusName === '售罄'){
-					return;
-				}
-				const productId = product.id;
-				this.loading = this.productModalShow = true;
-				this.detailScroll && this.detailScroll.scrollTo(0, 0, 500);
-				getProductDetail(productId).then(res => {
-					this.selectProduct = res || { spec: [], imgs: '' };
-					this.selectProduct.img = (this.selectProduct.imgs && this.selectProduct.imgs.length > 0) ? IMG_PATH + this.selectProduct.imgs.split(',')[0] : errorImg;
-					let obj = {};
-					this.specListData = [];
-					res.spec.forEach(e => {
-						const { type } = e;
-						if (!obj[type]) obj[type] = { type, list: [] };
-						obj[type].list.push(e);
-					});
-
-					for (let i in obj) {
-						let data = obj[i];
-						let selectSpec = data.list.find(e => {
-							return e.moreMoney === 0;
-						})
-						// data.selectSpec = data.list[0];
-						data.selectSpec = selectSpec;
-						this.specListData.push(data);
-					}
-					this.count = 1;
-					this.loading = false;
-					this.$nextTick(() => {
-						this.detailScroll && this.detailScroll.refresh();
-					});
-				});
-				if (this.firstShowDetail) {
-					this.firstShowDetail = false;
-					this.$nextTick(() => {
-						this.detailScroll = new BScroll('.detail-wrapper', scrollOption);
-					});
-				}
-			},
-			getName(lon, lat) {
-				this.gpsPoint = {
-					lon,
-					lat,
-				};
-				let json = gpsCovert.bd_encrypt(lat, lon);
-				let myGeo = new BMap.Geocoder();
-				// 根据坐标得到地址描述
-				myGeo.getLocation(new BMap.Point(json.lon, json.lat), result => {
-					if (result) {
-						this.address = result.address;
-					}
-				});
-			},
-			changeShop(val) {
-				this.loading = true;
-				const shopId = +val[0];
-				this.$store.commit('setShopInfo', this.shopList[0].find(i => i.value == shopId));
-				this.loadDataByOneShop(shopId);
-			},
-			getBanners(shopId) {
-				return getBanners(shopId).then(res => {
-					if (res) {
-						for (let i of res) {
-							this.bannerList.push({
-								url: i.hrefUrl,
-								img: IMG_PATH + i.imageKey,
-							});
-						}
-					}
-				});
-			},
-			getCategoryByShop(shopId) {
-				return getCategoryByShop(shopId).then(res => {
-					this.categories = res || [];
-					const category = this.categories[0] || {};
-					this.selectCategory(category.id, 0);
-				});
-			},
-			loadDataByOneShop(shopId) {
-				//产品清空
-				this.products = [];
-				this.bannerList = [];
-				// Promise.all([this.getBanners(shopId), this.getCategoryByShop(shopId)]).then(() => {
-				// 	this.loading = false;
-				// });
-				Promise.all([this.getBanners(shopId), this.getGoodsByShop(shopId)]).then(() => {
-					this.$nextTick(() => {
-						this._initSroll();
-						this._cacluateHeight();
-					});
-
-					this.loading = false;
-				});
-			},
-			getGoodsByShop(shopId) {
-				return getAllGoods(shopId).then(res => {
-					let goods = res;
-					goods.map(i => {
-						i.products.map(e => {
-							e.showImg = e.imgs && e.imgs.length > 0 ? (IMG_PATH + e.imgs.split(',')[0]) : errorImg;
-						});
-					});
-					this.goods = goods;
-				});
-			},
-			_initSroll() {
-				this.categoryScroll = new BScroll('.category-wrapper', {
-					click: true,
-				});
-				this.productScroll = new BScroll('.product-wrapper', {
-					click: true,
-					probeType: 3,
-				});
-				this.productScroll.on('scroll', pos => {
-					if (pos.y <= 0) {
-						this.scrollY = Math.abs(Math.round(pos.y));
-					}
-				});
-			},
-			_cacluateHeight() {
-				let productList = this.$refs.productList;
-				let height = 0;
-				this.listHeight.push(height);
-				for (let i = 0; i < productList.length; i++) {
-					let item = productList[i];
-					height += item.clientHeight;
-					this.listHeight.push(height);
-				}
-			},
-			followScroll(index) {
-				this.currentIndex = index;
-				let categoryList = this.$refs.categoryList;
-				let el = categoryList[index];
-				this.categoryScroll.scrollToElement(el, 300, 0, -100);
-			},
-			changeSelectProducts() {
-
-			},
-			showBigImg() {
-
-			},
-			getShop(lon,lat) {
-				getShopList({
-					start: 0,
-					length: 1000,
-				}).then(res => {
-					res = res || [];
-					if (res instanceof Array) {
-						for (let i of res) {
-							i.value = i.id;
-						}
-						this.shopList = [res];
-					} else {
-						res.value = res.id;
-						this.shopList = [[res]];
-					}
-					let nearestIndex=0;
-					for(let i=0;i<this.shopList.length;i++){
-						BMapLib.getDistance(this.shop)
-					}
-					this.$store.commit('setShopInfo', this.shopList[0][nearestIndex] || {});
+			});
+			if (this.firstShowDetail) {
+				this.firstShowDetail = false;
+				this.$nextTick(() => {
+					this.detailScroll = new BScroll('.detail-wrapper', scrollOption);
 				});
 			}
 		},
-		created() {
-			
+		getName(lon, lat) {
+			this.gpsPoint = {
+				lon,
+				lat,
+			};
+			let json = gpsCovert.bd_encrypt(lat, lon);
+			let myGeo = new BMap.Geocoder();
+			// 根据坐标得到地址描述
+			myGeo.getLocation(new BMap.Point(json.lon, json.lat), result => {
+				if (result) {
+					this.address = result.address;
+				}
+			});
 		},
-		mounted() {
-			// this.$nextTick(() => {
-			// 	new BScroll('.category-wrapper', scrollOption);
-			// 	this.productScroll = new BScroll('.product-wrapper', scrollOption);
-			// });
+		changeShop(val) {
 			this.loading = true;
+			const shopId = +val[0];
+			this.$store.commit('setShopInfo', this.shopList[0].find(i => i.value == shopId));
+			this.loadDataByOneShop(shopId);
+		},
+		getBanners(shopId) {
+			return getBanners(shopId).then(res => {
+				if (res) {
+					for (let i of res) {
+						this.bannerList.push({
+							url: i.hrefUrl,
+							img: IMG_PATH + i.imageKey,
+						});
+					}
+				}
+			});
+		},
+		getCategoryByShop(shopId) {
+			return getCategoryByShop(shopId).then(res => {
+				this.categories = res || [];
+				const category = this.categories[0] || {};
+				this.selectCategory(category.id, 0);
+			});
+		},
+		loadDataByOneShop(shopId) {
+			//产品清空
+			this.products = [];
+			this.bannerList = [];
+			// Promise.all([this.getBanners(shopId), this.getCategoryByShop(shopId)]).then(() => {
+			// 	this.loading = false;
+			// });
+			Promise.all([this.getBanners(shopId), this.getGoodsByShop(shopId)]).then(() => {
+				this.$nextTick(() => {
+					this._initSroll();
+					this._cacluateHeight();
+				});
+
+				this.loading = false;
+			});
+		},
+		getGoodsByShop(shopId) {
+			return getAllGoods(shopId).then(res => {
+				let goods = res;
+				goods.map(i => {
+					i.products.map(e => {
+						e.showImg = e.imgs && e.imgs.length > 0 ? IMG_PATH + e.imgs.split(',')[0] : errorImg;
+					});
+				});
+				this.goods = goods;
+			});
+		},
+		_initSroll() {
+			this.categoryScroll = new BScroll('.category-wrapper', {
+				click: true,
+			});
+			this.productScroll = new BScroll('.product-wrapper', {
+				click: true,
+				probeType: 3,
+			});
+			this.productScroll.on('scroll', pos => {
+				if (pos.y <= 0) {
+					this.scrollY = Math.abs(Math.round(pos.y));
+				}
+			});
+		},
+		_cacluateHeight() {
+			let productList = this.$refs.productList;
+			let height = 0;
+			this.listHeight.push(height);
+			for (let i = 0; i < productList.length; i++) {
+				let item = productList[i];
+				height += item.clientHeight;
+				this.listHeight.push(height);
+			}
+		},
+		followScroll(index) {
+			this.currentIndex = index;
+			let categoryList = this.$refs.categoryList;
+			let el = categoryList[index];
+			this.categoryScroll.scrollToElement(el, 300, 0, -100);
+		},
+		changeSelectProducts() {},
+		showBigImg() {},
+		getShop(lon, lat) {
+			return getShopList({
+				start: 0,
+				length: 1000,
+			}).then(res => {
+				res = res || [];
+				if (res instanceof Array) {
+					for (let i of res) {
+						i.value = i.id;
+					}
+					this.shopList = [res];
+				} else {
+					res.value = res.id;
+					this.shopList = [[res]];
+				}
+				let nearestIndex = 0;
+				let minDistance = 0;
+				let distance;
+				const json = gpsCovert.bd_encrypt(lat, lon);
+				const userPoint = new BMap.Point(json.lon, json.lat);
+				for (let i = 0; i < this.shopList.length; i++) {
+					const shopPoint = new BMap.Point(this.shopList[i].longitude, this.shopList[i].latitude);
+					distance = BMapLib.getDistance(userPoint, shopPoint);
+					if (i === 0) {
+						minDistance = distance;
+					} else if (minDistance > distance) {
+						nearestIndex = i;
+					}
+				}
+				console.log('最近距离' + nearestIndex);
+				this.$store.commit('setShopInfo', this.shopList[0][nearestIndex] || {});
+			});
+		},
+		getLocation() {
 			this.$wechat.ready(() => {
 				this.$wechat.getLocation({
 					type: 'gcj02', // 默认为wgs84的gps坐标，如果要返回直接给openLocation用的火星坐标，可传入'gcj02'
 					success: res => {
+						console.log('用户位置');
 						console.log(res);
 						this.getName(res.longitude, res.latitude);
+						this.getShop(res.longitude, res.latitude).then(res => {
+							this.loadDataByOneShop(this.shopInfo.id);
+						});
 					},
-					cancel: function (res) { },
+					cancel: function(res) {
+						this.getLocation();
+					},
 				});
 			});
-			// this.getShop(res.longitude, res.latitude);
 		},
-	};
+	},
+	created() {},
+	mounted() {
+		// this.$nextTick(() => {
+		// 	new BScroll('.category-wrapper', scrollOption);
+		// 	this.productScroll = new BScroll('.product-wrapper', scrollOption);
+		// });
+		this.loading = true;
+		this.getLocation();
+	},
+};
 </script>
 
